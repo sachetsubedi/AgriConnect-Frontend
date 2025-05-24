b2cc4a75320903c25cceb40c779524f68f0fbed3
sachetsubedi <sachetsubedi001@gmail.com>
2025-05-24 22:50:51 +0545

    feat: login page ui completed

--- app/auth/login/page.tsx
+++ app/auth/login/page.tsx
@@ -1,5 +1,130 @@
+"use client";
+import { Button } from "@/components/ui/button";
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { Icon } from "@iconify/react";
+import Link from "next/link";
+import { useState } from "react";
+import { useForm } from "react-hook-form";
+import z from "zod";
+
+const loginFormSchema = z.object({
+  email: z.string().email("Invalid email address"),
+  password: z.string().min(6, "Password must be at least 8 characters long"),
+});
+
 const LoginPage = () => {
-  return <div>LoginPage</div>;
+  const [isPasswordVisible, setIsPasswordVisible] = useState(false);
+
+  //
+  const form = useForm({
+    resolver: zodResolver(loginFormSchema),
+    defaultValues: {
+      email: "",
+      password: "",
+    },
+  });
+  return (
+    <div className="flex justify-between">
+      {/* Login form */}
+      <div className="w-[30%] flex flex-col items-center  h-screen">
+        <img src="/logo.png" alt="logo" className="mt-10" />
+        <Card className="w-[90%] mt-10 bg-blend-multiply bg-muted ">
+          <CardHeader>
+            <CardTitle className="text-2xl">User Login</CardTitle>
+          </CardHeader>
+          <CardContent className=" w-full">
+            <Form {...form}>
+              <form className="flex flex-col gap-5">
+                {/* ================================================================================================ */}
+                <FormField
+                  control={form.control}
+                  name="email"
+                  render={({ field }) => {
+                    return (
+                      <FormItem>
+                        <FormLabel>Email</FormLabel>
+                        <FormControl>
+                          <Input placeholder="example@gmail.com" {...field} />
+                        </FormControl>
+                        <FormMessage />
+                      </FormItem>
+                    );
+                  }}
+                />
+
+                {/* ================================================================================================ */}
+
+                <FormField
+                  control={form.control}
+                  name="password"
+                  render={({ field }) => {
+                    return (
+                      <FormItem>
+                        <FormLabel>Password</FormLabel>
+                        <FormControl>
+                          <div className="relative">
+                            <Input
+                              placeholder="Password"
+                              type={isPasswordVisible ? "text" : "password"}
+                              {...field}
+                            />
+                            <Icon
+                              icon={
+                                !isPasswordVisible
+                                  ? "solar:eye-closed-bold"
+                                  : "solar:eye-bold"
+                              }
+                              role="button"
+                              onClick={() => {
+                                setIsPasswordVisible(!isPasswordVisible);
+                              }}
+                              className="absolute right-3 top-1/2 -translate-y-1/2 cursor-pointer text-gray-500 z-20"
+                              width="24"
+                              height="24"
+                            />
+                          </div>
+                        </FormControl>
+                        <FormMessage />
+                      </FormItem>
+                    );
+                  }}
+                />
+
+                <div>
+                  Don&apos;t have an account?{" "}
+                  <Link
+                    href={"/#"}
+                    className="underline font-semibold text-primary"
+                  >
+                    {" "}
+                    Register here{" "}
+                  </Link>{" "}
+                </div>
+
+                {/* ================================================================================================ */}
+                <Button type="submit"> Login</Button>
+              </form>
+            </Form>
+          </CardContent>
+        </Card>
+      </div>
+
+      {/* banner */}
+      <div className="w-[70%] h-screen">
+        <img src={"/banner-2.png"} alt="banner" className="w-full h-screen" />
+      </div>
+    </div>
+  );
 };
 
 export default LoginPage;
