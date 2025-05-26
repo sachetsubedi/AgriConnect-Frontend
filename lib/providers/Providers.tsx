4b920fcb23edbd29c7ff7638f9f2dc27605c42c0
sachetsubedi <sachetsubedi001@gmail.com>
2025-05-26 21:56:34 +0545

    feat: add Toaster component for enhanced user notifications in Providers

--- lib/providers/Providers.tsx
+++ lib/providers/Providers.tsx
@@ -1,9 +1,15 @@
 "use client";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { FC, ReactNode } from "react";
+import { Toaster } from "sonner";
 const client = new QueryClient();
 const Providers: FC<{ children: ReactNode }> = ({ children }) => {
-  return <QueryClientProvider client={client}>{children}</QueryClientProvider>;
+  return (
+    <QueryClientProvider client={client}>
+      <Toaster richColors />
+      {children}
+    </QueryClientProvider>
+  );
 };
 
 export default Providers;
