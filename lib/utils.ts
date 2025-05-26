b716e277c5f33ec78a854eaf31a2ff9178742108
sachetsubedi <sachetsubedi001@gmail.com>
2025-05-26 21:51:43 +0545

    feat: enhance error handling in mapFieldsOnError function

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,6 +1,20 @@
-import { clsx, type ClassValue } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { AxiosError } from "axios";
+import { clsx, type ClassValue } from "clsx";
+import { twMerge } from "tailwind-merge";
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
 }
+
+export const mapFieldsOnError = (err: AxiosError<any>, setError: any) => {
+  if (err.response?.status !== 422) return;
+  const data = err?.response?.data?.data;
+  if (!data) return;
+  Object.entries(data).map(([key, value]) => {
+    // @ts-expect-error
+    const error = value[0];
+    setError(key, {
+      message: error,
+    });
+  });
+};
