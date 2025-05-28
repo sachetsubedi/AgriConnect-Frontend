68a318d32f3dfa74ad671cb1205b836752e5737b
sachetsubedi <sachetsubedi001@gmail.com>
2025-05-28 13:32:48 +0545

    feat: custom loader added

---
+++ components/Loader.tsx
@@ -0,0 +1,9 @@
+const Loader = () => {
+  return (
+    <div className="flex mt-20 items-center justify-center">
+      <div className="loader"></div>
+    </div>
+  );
+};
+
+export default Loader;
