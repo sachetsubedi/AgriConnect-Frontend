d17d4d4e9dafd9408aca7149284e6bd7547571a2
sachetsubedi <sachetsubedi001@gmail.com>
2025-06-13 14:22:43 +0545

    feat: sidebar init for notifications

--- app/p/[userId]/layout.tsx
+++ app/p/[userId]/layout.tsx
@@ -1,18 +1,17 @@
 import { AppSidebar } from "@/components/app-sidebar";
-import {
-  Breadcrumb,
-  BreadcrumbItem,
-  BreadcrumbLink,
-  BreadcrumbList,
-  BreadcrumbPage,
-  BreadcrumbSeparator,
-} from "@/components/ui/breadcrumb";
 import { Separator } from "@/components/ui/separator";
+import {
+  Sheet,
+  SheetContent,
+  SheetTitle,
+  SheetTrigger,
+} from "@/components/ui/sheet";
 import {
   SidebarInset,
   SidebarProvider,
   SidebarTrigger,
 } from "@/components/ui/sidebar";
+import { Bell } from "lucide-react";
 import { use } from "react";
 
 export default function UserLayout({
@@ -28,24 +27,21 @@ export default function UserLayout({
       <SidebarProvider>
         <AppSidebar userId={rparams.userId} />
         <SidebarInset>
-          <header className="flex h-16 shrink-0 items-center gap-2 transition-[width,height] ease-linear group-has-[[data-collapsible=icon]]/sidebar-wrapper:h-12">
+          <header className="flex h-16 shrink-0 items-center justify-between gap-2 transition-[width,height] ease-linear group-has-[[data-collapsible=icon]]/sidebar-wrapper:h-12">
             <div className="flex items-center gap-2 px-4">
               <SidebarTrigger className="-ml-1" />
               <Separator orientation="vertical" className="mr-2 h-4" />
-              <Breadcrumb>
-                <BreadcrumbList>
-                  <BreadcrumbItem className="hidden md:block">
-                    <BreadcrumbLink href="#">
-                      Building Your Application
-                    </BreadcrumbLink>
-                  </BreadcrumbItem>
-                  <BreadcrumbSeparator className="hidden md:block" />
-                  <BreadcrumbItem>
-                    <BreadcrumbPage>Data Fetching</BreadcrumbPage>
-                  </BreadcrumbItem>
-                </BreadcrumbList>
-              </Breadcrumb>
             </div>
+            <Sheet>
+              <SheetTrigger className="pr-5">
+                <Bell />
+              </SheetTrigger>
+              <SheetContent>
+                <SheetTitle>Notifications</SheetTitle>
+                <Separator className="my-2" />
+                asd
+              </SheetContent>
+            </Sheet>
           </header>
           <div className="flex flex-1 flex-col gap-4 p-4 pt-0 px-16">
             {children}
